ba1f027efd42e08f7b1f9783fdd3966b273d5d28
soufiane.elkharmo <soufiane.elkharmo@hes-so.ch>
2023-04-11 21:17:38 +0200

    scuola

--- William/main.c
+++ William/main.c
@@ -6,7 +6,6 @@
 #include <stdbool.h>
 #include <time.h>
 
-
 // Sem for producer
 sem_t sem_driver;
 sem_t sem_military;
@@ -17,26 +16,48 @@ sem_t sem_plane;
 sem_t sem_truck;
 sem_t sem_boat;
 
+int convoi_id = 0;
+pthread_mutex_t mutex_convoi_id;
+pthread_mutex_t mutex_delete;
 int buffer[6][5] = {0}; // Initializing all values to 0
 
-void put(int weight, int range, int col_index) {
+void put(int weight, int range, int col_index)
+{
     int timeToWait = (rand() % 2 == 0) ? 8 : 12;
-    printf("time to wait : %d\n",timeToWait);
-    usleep(1000* timeToWait);
+    printf("time to wait : %d\n", timeToWait);
+    usleep(1000 * timeToWait);
+    int sem_value;
 
-    for (int i = 0; i < 6; i++) {
-        if (buffer[i][col_index] == 0) {
+    for (int i = 0; i < 6; i++)
+    {
+
+        if (buffer[i][col_index] == 0)
+        {
             int random_num = rand() % (2 * range + 1) - range; // Générer un nombre aléatoire entre -range et range
-            buffer[i][col_index] = weight + random_num; // Ajouter le nombre aléatoire au poids
-            if(buffer[i][0] != 0 && buffer[i][1] != 0 && buffer[i][2] != 0){
+            buffer[i][col_index] = weight + random_num;        // Ajouter le nombre aléatoire au poids
+            if (buffer[i][0] != 0 && buffer[i][1] != 0 && buffer[i][2] != 0)
+            {
                 buffer[i][3] = buffer[i][0] + buffer[i][1] + buffer[i][2];
-                buffer[i][4] = time(NULL);
-                if(buffer[i][3]<10001){
+                pthread_mutex_lock(&mutex_convoi_id);
+                buffer[i][4] = ++convoi_id;
+                pthread_mutex_unlock(&mutex_convoi_id);
+                if (buffer[i][3] < 10001)
+                {
                     sem_post(&sem_plane);
-                } else if(buffer[i][3]>10000 && buffer[i][3]<30001){
+                    sem_getvalue(&sem_plane, &sem_value);
+                    printf("Plane sem: %d\n", sem_value);
+                }
+                else if (buffer[i][3] > 10000 && buffer[i][3] < 30001)
+                {
                     sem_post(&sem_truck);
-                } else if(buffer[i][3]>30000){
+                    sem_getvalue(&sem_truck, &sem_value);
+                    printf("Truck sem: %d\n", sem_value);
+                }
+                else if (buffer[i][3] > 30000)
+                {
                     sem_post(&sem_boat);
+                    sem_getvalue(&sem_boat, &sem_value);
+                    printf("Boat sem: %d\n", sem_value);
                 }
             }
             break;
@@ -44,109 +65,177 @@ void put(int weight, int range, int col_index) {
     }
 }
 
+void get(void *arg)
+{
 
-void* consumer(void* arg) {
-    char* name = (char*) arg;
+    char *name = (char *)arg;
+    int convoi_nb = 300;
+    int tmp_i;
+    // Attente du sémaphore approprié
+    while (true)
+    {
+        if (strcmp(name, "plane") == 0)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (buffer[i][3] < 100000)
+                    if (buffer[i][4] < convoi_nb)
+                    {
+                        convoi_nb = buffer[i][4];
+                        tmp_i = i;
+                        printf("Tmp_i_1 %d", tmp_i);
+                    }
+
+                pthread_mutex_lock(&mutex_delete);
+                for (int i = 0; i < 5; i++)
+                {
+                    buffer[tmp_i][i] = 0;
+                    printf("Tmp_i_2 %d", tmp_i);
+                }
+                sem_post(&sem_driver);
+                sem_post(&sem_military);
+                sem_post(&sem_material);
+
+                pthread_mutex_unlock(&mutex_delete);
+
+                printf("Plane is going\n");
+            }
+        }
+    }
+}
+void *consumer(void *arg)
+{
+    char *name = (char *)arg;
 
     // Attente du sémaphore approprié
-    while(true){
-        if (strcmp(name, "plane") == 0) {
+    while (true)
+    {
+        if (strcmp(name, "plane") == 0)
+        {
             sem_wait(&sem_plane);
+            get("plane");
             printf("Plane is going\n");
-
-        } else if (strcmp(name, "truck") == 0) {
+        }
+        else if (strcmp(name, "truck") == 0)
+        {
             sem_wait(&sem_truck);
             printf("Truck is going\n");
-        } else if (strcmp(name, "boat") == 0) {
+        }
+        else if (strcmp(name, "boat") == 0)
+        {
             sem_wait(&sem_boat);
             printf("Boat is going\n");
         }
     }
 }
 
-void* producer(void* arg) {
-    char* name = (char*) arg;
+void *producer(void *arg)
+{
+    char *name = (char *)arg;
 
     // Attente du sémaphore approprié
-    while(true){
-        if (strcmp(name, "driver") == 0) {
+    while (true)
+    {
+        if (strcmp(name, "driver") == 0)
+        {
             sem_wait(&sem_driver);
-            put(80,20, 0);
-
-        } else if (strcmp(name, "military") == 0) {
+            put(80, 20, 0);
+        }
+        else if (strcmp(name, "military") == 0)
+        {
             sem_wait(&sem_military);
-            put(8000,7000, 1);
-        } else if (strcmp(name, "material") == 0) {
+            put(8000, 7000, 1);
+        }
+        else if (strcmp(name, "material") == 0)
+        {
             sem_wait(&sem_material);
-            put(25000,24500, 2);
+            put(25000, 24500, 2);
         }
 
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
                 printf("%d ", buffer[i][j]);
             }
-        printf("\n");
+            printf("\n");
         }
         printf("\n");
         printf("\n");
         printf("\n");
     }
-
 
     printf("Starting %s producer\n", name);
     // Code de production ici
     printf("Exiting %s producer\n", name);
 
     // Libération du sémaphore approprié
-    if (strcmp(name, "driver") == 0) {
+    if (strcmp(name, "driver") == 0)
+    {
         sem_post(&sem_driver);
-    } else if (strcmp(name, "military") == 0) {
+    }
+    else if (strcmp(name, "military") == 0)
+    {
         sem_post(&sem_military);
-    } else if (strcmp(name, "material") == 0) {
+    }
+    else if (strcmp(name, "material") == 0)
+    {
         sem_post(&sem_material);
     }
 
     pthread_exit(NULL);
 }
 
-int main() {
-    
+int main()
+{
+
+    pthread_mutex_init(&mutex_convoi_id, NULL);
+    pthread_mutex_init(&mutex_delete, NULL);
+
     // Loop to print all values in the buffer
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
             printf("%d ", buffer[i][j]);
         }
         printf("\n");
     }
 
     // Initialisation des sémaphores producer
-    if (sem_init(&sem_driver, 0, 6) != 0) {
+    if (sem_init(&sem_driver, 0, 6) != 0)
+    {
         printf("Error initializing semaphore for driver producer\n");
         exit(EXIT_FAILURE);
     }
 
-    if (sem_init(&sem_military, 0, 6) != 0) {
+    if (sem_init(&sem_military, 0, 6) != 0)
+    {
         printf("Error initializing semaphore for military producer\n");
         exit(EXIT_FAILURE);
     }
 
-    if (sem_init(&sem_material, 0, 6) != 0) {
+    if (sem_init(&sem_material, 0, 6) != 0)
+    {
         printf("Error initializing semaphore for material producer\n");
         exit(EXIT_FAILURE);
     }
 
-        // Initialisation des sémaphores consumer
-    if (sem_init(&sem_plane, 0, 0) != 0) {
+    // Initialisation des sémaphores consumer
+    if (sem_init(&sem_plane, 0, 0) != 0)
+    {
         printf("Error initializing semaphore for driver producer\n");
         exit(EXIT_FAILURE);
     }
 
-    if (sem_init(&sem_truck, 0, 0) != 0) {
+    if (sem_init(&sem_truck, 0, 0) != 0)
+    {
         printf("Error initializing semaphore for military producer\n");
         exit(EXIT_FAILURE);
     }
 
-    if (sem_init(&sem_boat, 0, 0) != 0) {
+    if (sem_init(&sem_boat, 0, 0) != 0)
+    {
         printf("Error initializing semaphore for material producer\n");
         exit(EXIT_FAILURE);
     }
@@ -179,6 +268,9 @@ int main() {
     sem_destroy(&sem_plane);
     sem_destroy(&sem_truck);
     sem_destroy(&sem_boat);
+
+    pthread_mutex_destroy(&mutex_convoi_id);
+    pthread_mutex_destroy(&mutex_delete);
 
     printf("All producer threads have exited\n");
 
