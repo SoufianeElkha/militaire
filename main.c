970609ee4fb150e559e81e12d609c59c5d602f98
winkal <winkal@winkal.myguest.virtualbox.org>
2023-04-14 11:28:54 +0200

    Commenti in Inglese

--- main.c
+++ main.c
@@ -8,16 +8,20 @@
 #include <unistd.h>
 #include <limits.h>
 
+// Define constants for the number of producers, consumers, and convoys
 #define NUM_PRODUCERS 3
 #define NUM_CONSUMERS 3
 #define NUM_CONVOIS 200
 
+// Declare semaphores for producers and consumers
 sem_t sem_driver, sem_military, sem_material;
 sem_t sem_plane, sem_truck, sem_boat;
 
+// Declare mutexes for convoy ID, number of convoys processed, buffer access, and consumers_done flag
 pthread_mutex_t mutex_convoi_id, mutex_convois_processed;
 pthread_mutex_t mutex_buffer, mutex_consumers_done;
 
+// Define producer types
 typedef enum
 {
     DRIVER,
@@ -25,6 +29,7 @@ typedef enum
     MATERIAL
 } producer_type;
 
+// Define consumer types
 typedef enum
 {
     PLANE,
@@ -32,15 +37,19 @@ typedef enum
     BOAT
 } consumer_type;
 
+// Declare global variables for convoy counts, convoy ID, and buffer
 int convois_processed = 0;
 int convoi_id = 0;
 int plane_count = 0;
 int truck_count = 0;
 int boat_count = 0;
 int buffer[6][5] = {0};
+
+// Declare global variables for the done flag and consumers_done count
 bool done = false;
 int consumers_done = 0;
 
+// Function to print the current state of the buffer
 void print_buffer()
 {
     printf("PILOTE-MILITARY-MATERIAL-TOTAL-NUMERO\n");
@@ -53,21 +62,29 @@ void print_buffer()
     printf("\n\n");
 }
 
+/// @brief Function to place an item in the buffer
+/// @param weight
+/// @param range
+/// @param col_index
 void put(int weight, int range, int col_index)
 {
-    int first_empty_row = -1;
-    static int counts[3] = {0, 0, 0}; // 0: driver_count, 1: military_count, 2: material_count
-    int timeToWait = (rand() % 2 == 0) ? 8 : 12;
-    usleep(1000 * timeToWait);
-    pthread_mutex_lock(&mutex_buffer);
+    int first_empty_row = -1; // Initialize first empty row as -1
+    // Array to hold the counts of drivers, military personnel, and materials
+    static int counts[3] = {0, 0, 0};            // 0: driver_count, 1: military_count, 2: material_count
+    int timeToWait = (rand() % 2 == 0) ? 8 : 12; // Randomly select a wait time of either 8 or 12
+    usleep(1000 * timeToWait);                   // Sleep for the randomly selected wait time in microseconds
 
+    pthread_mutex_lock(&mutex_buffer); // Lock the mutex for the buffer
+
+    // Check if the count for the current column has reached its limit
     if (done) //(counts[col_index] >= 218)
     {
-        pthread_mutex_unlock(&mutex_buffer);
+        pthread_mutex_unlock(&mutex_buffer); // Unlock the mutex and return
         return;
     }
-    counts[col_index]++;
+    counts[col_index]++; // Increment the count for the current column
 
+    // Find the first empty row in the buffer for the current column
     for (int i = 0; i < 6; i++)
     {
         if (buffer[i][0] == 0 || buffer[i][1] == 0 || buffer[i][2] == 0)
@@ -80,41 +97,54 @@ void put(int weight, int range, int col_index)
         }
     }
 
+    // If an empty row is found
     if (first_empty_row != -1)
     {
+        // Generate a random number within the specified range
         int random_num = rand() % (2 * range + 1) - range;
+        // Assign the weight with the random number added to the buffer
         buffer[first_empty_row][col_index] = weight + random_num;
 
+        // Check if the current row is completely filled
         if (buffer[first_empty_row][0] != 0 && buffer[first_empty_row][1] != 0 && buffer[first_empty_row][2] != 0)
         {
+            // Calculate the total weight for the current row
             buffer[first_empty_row][3] = buffer[first_empty_row][0] + buffer[first_empty_row][1] + buffer[first_empty_row][2];
 
+            // Lock the mutex for convoi_id
             pthread_mutex_lock(&mutex_convoi_id);
+            // Increment and assign the convoi_id
             buffer[first_empty_row][4] = ++convoi_id;
+            // Unlock the mutex for convoi_id
             pthread_mutex_unlock(&mutex_convoi_id);
 
+            // Determine the type of transportation for the current row based on the total weight
             if (buffer[first_empty_row][3] < 10001)
             {
-                sem_post(&sem_plane);
+                sem_post(&sem_plane); // Signal that a plane is available
             }
             else if (buffer[first_empty_row][3] > 10001 && buffer[first_empty_row][3] < 30001)
             {
-                sem_post(&sem_truck);
+                sem_post(&sem_truck); // Signal that a truck is available
             }
             else if (buffer[first_empty_row][3] > 30001)
             {
-                sem_post(&sem_boat);
+                sem_post(&sem_boat); // Signal that a boat is available
             }
         }
     }
+    // Unlock the mutex for the buffer
     pthread_mutex_unlock(&mutex_buffer);
 }
 
+/// @brief Function to retrieve and process a convoy based on its transportation type (plane, truck, or boat)
+/// @param name
 void get(const char *name)
 {
-    int tmp_i = -1;
-    int lower_bound, upper_bound;
+    int tmp_i = -1;               // Initialize temporary index as -1
+    int lower_bound, upper_bound; // Declare lower and upper bounds for transportation type
 
+    // Set the bounds and increment counts based on the transportation type
     if (strcmp(name, "plane") == 0)
     {
         lower_bound = 0;
@@ -135,21 +165,23 @@ void get(const char *name)
     }
     else
     {
-        return;
+        return; // Return if the transportation type is not recognized
     }
 
-    pthread_mutex_lock(&mutex_buffer);
+    pthread_mutex_lock(&mutex_buffer); // Lock the mutex for the buffer
 
     // Find the appropriate convoy
     for (int i = 0; i < 6; i++)
     {
-        if (buffer[i][0] != 0 && buffer[i][1] != 0 && buffer[i][2] != 0) // Check if the row is complete
+        // Check if the row is complete
+        if (buffer[i][0] != 0 && buffer[i][1] != 0 && buffer[i][2] != 0)
         {
-            int current_weight = buffer[i][3];
-            int current_convoi_nb = buffer[i][4];
+            int current_weight = buffer[i][3];    // Get the current weight
+            int current_convoi_nb = buffer[i][4]; // Get the current convoy number
+            // Check if the convoy falls within the bounds for the given transportation type
             if (current_weight >= lower_bound && current_weight < upper_bound && current_convoi_nb <= NUM_CONVOIS && current_convoi_nb > 0)
             {
-                tmp_i = i;
+                tmp_i = i; // Store the index of the matching convoy
             }
         }
     }
@@ -166,20 +198,24 @@ void get(const char *name)
         sem_post(&sem_material);
 
         // Increase the count of processed convoys
-        pthread_mutex_lock(&mutex_convois_processed);
+        pthread_mutex_lock(&mutex_convois_processed); // Lock the mutex for convois_processed
         convois_processed++;
-        printf("\n######################\nConvoi terminati: %d\n######################\n", convois_processed);
-        pthread_mutex_unlock(&mutex_convois_processed);
+        printf("\n######################\nConvoi terminati: %d\n######################\n", convois_processed); // Print the number of convoys processed
+        pthread_mutex_unlock(&mutex_convois_processed);                                                        // Unlock the mutex for convois_processed
     }
 
-    pthread_mutex_unlock(&mutex_buffer);
+    pthread_mutex_unlock(&mutex_buffer); // Unlock the mutex for the buffer
 }
 
+/// @brief Function for the producer threads (driver, military, and material)
+/// @param arg
+/// @return
 void *producer(void *arg)
 {
-    char *name = (char *)arg;
-    producer_type p_type;
+    char *name = (char *)arg; // Get the producer name from the argument
+    producer_type p_type;     // Declare the producer type
 
+    // Set the producer type based on the name
     if (strcmp(name, "driver") == 0)
         p_type = DRIVER;
     else if (strcmp(name, "military") == 0)
@@ -187,14 +223,15 @@ void *producer(void *arg)
     else if (strcmp(name, "material") == 0)
         p_type = MATERIAL;
     else
-        return NULL;
+        return NULL; // Return NULL if the producer type is not recognized
 
+    // Continue running until the 'done' flag is set
     while (!done)
     {
+        int weight, range, col_index; // Declare weight, range, and column index variables
+        sem_t *semaphore;             // Declare the semaphore pointer
 
-        int weight, range, col_index;
-        sem_t *semaphore;
-
+        // Set weight, range, column index, and semaphore based on the producer type
         switch (p_type)
         {
         case DRIVER:
@@ -217,47 +254,52 @@ void *producer(void *arg)
             break;
         }
 
+        // Check if the maximum number of convoys has been reached
         if (convois_processed >= NUM_CONVOIS)
         {
             pthread_mutex_lock(&mutex_consumers_done);
-            done = true;
+            done = true; // Set the 'done' flag
             pthread_mutex_unlock(&mutex_consumers_done);
             printf("2 NUMERO MASSIMO RAGGIUNTO\n");
-            break;
+            break; // Break the loop
         }
         else
         {
-            sem_wait(semaphore);
+            sem_wait(semaphore); // Wait for the semaphore
 
-            // Aggiungi questo controllo
+            // Check if the maximum number of convoys has been reached while waiting for the semaphore
             pthread_mutex_lock(&mutex_convois_processed);
             if (convois_processed >= NUM_CONVOIS)
             {
                 pthread_mutex_unlock(&mutex_convois_processed);
-                sem_post(semaphore);
-                break;
+                sem_post(semaphore); // Release the semaphore
+                break;               // Break the loop
             }
             pthread_mutex_unlock(&mutex_convois_processed);
 
-            put(weight, range, col_index);
+            put(weight, range, col_index); // Call the 'put' function with the calculated weight, range, and column index
         }
 
-        print_buffer();
-        printf("Exiting %s producer\n", name);
+        print_buffer();                        // Print the buffer
+        printf("Exiting %s producer\n", name); // Print a message indicating the producer is exiting
     }
 
     // Release the appropriate semaphore
     sem_post(&sem_driver);
     sem_post(&sem_military);
     sem_post(&sem_material);
-    return NULL;
+    return NULL; // Return NULL as the function return value
 }
 
+/// @brief Function for the consumer threads (plane, truck, and boat)
+/// @param arg
+/// @return
 void *consumer(void *arg)
 {
-    const char *name = (const char *)arg;
-    consumer_type c_type;
+    const char *name = (const char *)arg; // Get the consumer name from the argument
+    consumer_type c_type;                 // Declare the consumer type
 
+    // Set the consumer type based on the name
     if (strcmp(name, "plane") == 0)
         c_type = PLANE;
     else if (strcmp(name, "truck") == 0)
@@ -265,14 +307,15 @@ void *consumer(void *arg)
     else if (strcmp(name, "boat") == 0)
         c_type = BOAT;
     else
-        return NULL;
+        return NULL; // Return NULL if the consumer type is not recognized
 
+    // Continue running until the 'done' flag is set
     while (!done)
     {
+        sem_t *semaphore;   // Declare the semaphore pointer
+        const char *c_name; // Declare the consumer name
 
-        sem_t *semaphore;
-        const char *c_name;
-
+        // Set the semaphore and consumer name based on the consumer type
         switch (c_type)
         {
         case PLANE:
@@ -289,109 +332,160 @@ void *consumer(void *arg)
             break;
         }
 
+        // Check if the maximum number of convoys has been reached
         if (convois_processed >= NUM_CONVOIS)
         {
             pthread_mutex_lock(&mutex_consumers_done);
-            done = true;
+            done = true; // Set the 'done' flag
             pthread_mutex_unlock(&mutex_consumers_done);
             printf("2 NUMERO MASSIMO RAGGIUNTO\n");
-            break;
+            break; // Break the loop
         }
         else
         {
-            sem_wait(semaphore);
+            sem_wait(semaphore); // Wait for the semaphore
 
-            // Aggiungi questo controllo
+            // Check if the maximum number of convoys has been reached while waiting for the semaphore
             pthread_mutex_lock(&mutex_convois_processed);
             if (convois_processed >= NUM_CONVOIS)
             {
                 pthread_mutex_unlock(&mutex_convois_processed);
-                sem_post(semaphore);
-                break;
+                sem_post(semaphore); // Release the semaphore
+                break;               // Break the loop
             }
             pthread_mutex_unlock(&mutex_convois_processed);
-            get(c_name);
+
+            get(c_name); // Call the 'get' function with the consumer name
         }
 
-        // Release the mutex after accessing the buffer
-        // pthread_mutex_unlock(&mutex_buffer);
-
-        printf("%s is going\n", c_name);
+        printf("%s is going\n", c_name); // Print a message indicating the consumer is running
     }
+
+    // Release the appropriate semaphore
     sem_post(&sem_plane);
     sem_post(&sem_truck);
     sem_post(&sem_boat);
 
-    return NULL;
+    return NULL; // Return NULL as the function return value
 }
 
+/// @brief main
+/// @return
 int main()
 {
-    pthread_mutex_init(&mutex_convoi_id, NULL);
-    pthread_mutex_init(&mutex_convois_processed, NULL);
-    pthread_mutex_init(&mutex_buffer, NULL);
-    pthread_mutex_init(&mutex_consumers_done, NULL);
+    // Initialize mutexes
+    int init_result;
 
+    init_result = pthread_mutex_init(&mutex_convoi_id, NULL);
+    if (init_result != 0)
+    {
+        fprintf(stderr, "Error initializing mutex_convoi_id. Error code: %d\n", init_result);
+        exit(EXIT_FAILURE);
+    }
+
+    init_result = pthread_mutex_init(&mutex_convois_processed, NULL);
+    if (init_result != 0)
+    {
+        fprintf(stderr, "Error initializing mutex_convois_processed. Error code: %d\n", init_result);
+        exit(EXIT_FAILURE);
+    }
+
+    init_result = pthread_mutex_init(&mutex_buffer, NULL);
+    if (init_result != 0)
+    {
+        fprintf(stderr, "Error initializing mutex_buffer. Error code: %d\n", init_result);
+        exit(EXIT_FAILURE);
+    }
+
+    init_result = pthread_mutex_init(&mutex_consumers_done, NULL);
+    if (init_result != 0)
+    {
+        fprintf(stderr, "Error initializing mutex_consumers_done. Error code: %d\n", init_result);
+        exit(EXIT_FAILURE);
+    }
+
+    // Declare arrays for semaphores, thread names, and thread handles
     sem_t *producer_sems[NUM_PRODUCERS] = {&sem_driver, &sem_military, &sem_material};
     sem_t *consumer_sems[NUM_CONSUMERS] = {&sem_plane, &sem_truck, &sem_boat};
 
     const char *producer_names[] = {"driver", "military", "material"};
     const char *consumer_names[] = {"plane", "truck", "boat"};
 
-    pthread_t producer_threads[NUM_PRODUCERS], consumer_threads[NUM_CONSUMERS];
+    pthread_t producer_threads[NUM_PRODUCERS];
+    pthread_t consumer_threads[NUM_CONSUMERS];
 
     // Initialize semaphores and create producer and consumer threads
     for (int i = 0; i < NUM_PRODUCERS; ++i)
     {
+        // Initialize producer semaphore
         if (sem_init(producer_sems[i], 0, 6) != 0)
         {
             printf("Error initializing semaphore for %s producer\n", producer_names[i]);
             exit(EXIT_FAILURE);
         }
 
-        pthread_create(&producer_threads[i], NULL, producer, (void *)producer_names[i]);
-
+        // Initialize consumer semaphore
         if (sem_init(consumer_sems[i], 0, 0) != 0)
         {
             printf("Error initializing semaphore for %s consumer\n", consumer_names[i]);
             exit(EXIT_FAILURE);
         }
 
-        pthread_create(&consumer_threads[i], NULL, consumer, (void *)consumer_names[i]);
+        // Create producer thread
+        int producer_thread_creation = pthread_create(&producer_threads[i], NULL, producer, (void *)producer_names[i]);
+        if (producer_thread_creation != 0)
+        {
+            printf("Error creating %s producer thread. Error code: %d\n", producer_names[i], producer_thread_creation);
+            exit(EXIT_FAILURE);
+        }
+
+        // Create consumer thread
+        int consumer_thread_creation = pthread_create(&consumer_threads[i], NULL, consumer, (void *)consumer_names[i]);
+        if (consumer_thread_creation != 0)
+        {
+            printf("Error creating %s consumer thread. Error code: %d\n", consumer_names[i], consumer_thread_creation);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    // Join producer threads
+    // Join producer and consumer threads
     for (int i = 0; i < NUM_PRODUCERS; ++i)
     {
-        pthread_join(producer_threads[i], NULL);
-    }
-    // Join consumer threads
-    for (int i = 0; i < NUM_CONSUMERS; ++i)
-    {
-        pthread_join(consumer_threads[i], NULL);
+        int producer_join_result = pthread_join(producer_threads[i], NULL);
+        if (producer_join_result != 0)
+        {
+            printf("Error joining %s producer thread. Error code: %d\n", producer_names[i], producer_join_result);
+            exit(EXIT_FAILURE);
+        }
+
+        int consumer_join_result = pthread_join(consumer_threads[i], NULL);
+        if (consumer_join_result != 0)
+        {
+            printf("Error joining %s consumer thread. Error code: %d\n", consumer_names[i], consumer_join_result);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    // Destroy semaphores producer
+    // Destroy producer and consumer semaphores
     for (int i = 0; i < NUM_PRODUCERS; ++i)
     {
         sem_destroy(producer_sems[i]);
-    }
-    // Destroy semaphores consumer
-    for (int i = 0; i < NUM_CONSUMERS; ++i)
-    {
         sem_destroy(consumer_sems[i]);
     }
 
+    // Destroy mutexes
     pthread_mutex_destroy(&mutex_convoi_id);
     pthread_mutex_destroy(&mutex_convois_processed);
     pthread_mutex_destroy(&mutex_buffer);
     pthread_mutex_destroy(&mutex_consumers_done);
 
+    // Print results
     printf("\nAll producer threads have exited\n");
-    printf("Convoi eseguiti: %d\n", convois_processed);
-    printf("Plane partiti: %d\n", plane_count);
-    printf("Truck partiti: %d\n", truck_count);
-    printf("Boat partiti: %d\n", boat_count);
-    printf("Somma partiti ++ : %d\n", plane_count + truck_count + boat_count);
+    printf("Convoys executed: %d\n", convois_processed);
+    printf("Planes dispatched: %d\n", plane_count);
+    printf("Trucks dispatched: %d\n", truck_count);
+    printf("Boats dispatched: %d\n", boat_count);
+    printf("Total dispatched: %d\n", plane_count + truck_count + boat_count);
+
     return 0;
 }
