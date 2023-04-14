66bd4a91e9eeb1cb580d47521b9f9c494d3cf8b1
winkal <winkal@winkal.myguest.virtualbox.org>
2023-04-14 07:39:40 +0200

    vba

--- organigrammaPP.vb
+++ organigrammaPP.vb
@@ -4,8 +4,9 @@ Sub CreateOrganigram()
     Dim pptApp As PowerPoint.Application
     Dim pptPres As PowerPoint.Presentation
     Dim pptSlide As PowerPoint.Slide
-    Dim producerShape As PowerPoint.Shape
-    Dim consumerShape As PowerPoint.Shape
+    Dim smartArtLayout As SmartArtLayout
+    Dim smartArt As PowerPoint.Shape
+    Dim node As SmartArtNode
     
     ' Crea una nuova applicazione PowerPoint e una nuova presentazione
     Set pptApp = New PowerPoint.Application
@@ -13,25 +14,34 @@ Sub CreateOrganigram()
     Set pptPres = pptApp.Presentations.Add
     Set pptSlide = pptPres.Slides.Add(1, ppLayoutBlank)
     
-    ' Aggiungi la forma Producers e il testo
-    Set producerShape = pptSlide.Shapes.AddTextbox(msoTextOrientationHorizontal, 50, 50, 300, 100)
-    With producerShape.TextFrame.TextRange
-        .Text = "Producers" & vbCrLf & _
-                "a. Creazione di 3 produttori: driver, military e material" & vbCrLf & _
-                "b. Ogni produttore attende il semaforo corrispondente (sem_driver, sem_military o sem_material)" & vbCrLf & _
-                "c. Una volta ottenuto il semaforo, il produttore chiama la funzione put(weight, range, col_index) con i parametri appropriati" & vbCrLf & _
-                "d. La funzione put:"
-        .ParagraphFormat.SpaceAfter = 10
+    ' Scegli un layout di organigramma SmartArt
+    Set smartArtLayout = pptApp.SmartArtLayouts(88) 'Organigramma orizzontale
+    
+    ' Aggiungi lo SmartArt all'interno della diapositiva
+    Set smartArt = pptSlide.Shapes.AddSmartArt(smartArtLayout, 50, 50, 600, 400)
+    
+    ' Modifica il testo delle forme dello SmartArt
+    With smartArt.SmartArt.Nodes(1)
+        .TextFrame2.TextRange.Text = "Producers"
+        Set node = .Nodes.Add
+        node.TextFrame2.TextRange.Text = "a. Creazione di 3 produttori: driver, military e material"
+        Set node = .Nodes.Add
+        node.TextFrame2.TextRange.Text = "b. Ogni produttore attende il semaforo corrispondente (sem_driver, sem_military o sem_material)"
+        Set node = .Nodes.Add
+        node.TextFrame2.TextRange.Text = "c. Una volta ottenuto il semaforo, il produttore chiama la funzione put(weight, range, col_index) con i parametri appropriati"
+        Set node = .Nodes.Add
+        node.TextFrame2.TextRange.Text = "d. La funzione put:"
     End With
     
-    ' Aggiungi la forma Consumers e il testo
-    Set consumerShape = pptSlide.Shapes.AddTextbox(msoTextOrientationHorizontal, 50, 200, 300, 100)
-    With consumerShape.TextFrame.TextRange
-        .Text = "Consumers" & vbCrLf & _
-                "a. Creazione di 3 consumatori: plane, truck e boat" & vbCrLf & _
-                "b. Ogni consumatore attende il semaforo corrispondente (sem_plane, sem_truck o sem_boat)" & vbCrLf & _
-                "c. Una volta ottenuto il semaforo, il consumatore chiama la funzione get(const char *name) con il nome appropriato" & vbCrLf & _
-                "d. La funzione get:"
-        .ParagraphFormat.SpaceAfter = 10
+    With smartArt.SmartArt.Nodes(2)
+        .TextFrame2.TextRange.Text = "Consumers"
+        Set node = .Nodes.Add
+        node.TextFrame2.TextRange.Text = "a. Creazione di 3 consumatori: plane, truck e boat"
+        Set node = .Nodes.Add
+        node.TextFrame2.TextRange.Text = "b. Ogni consumatore attende il semaforo corrispondente (sem_plane, sem_truck o sem_boat)"
+        Set node = .Nodes.Add
+        node.TextFrame2.TextRange.Text = "c. Una volta ottenuto il semaforo, il consumatore chiama la funzione get(const char *name) con il nome appropriato"
+        Set node = .Nodes.Add
+        node.TextFrame2.TextRange.Text = "d. La funzione get:"
     End With
 End Sub
